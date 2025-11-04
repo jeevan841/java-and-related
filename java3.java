3388f16367dd76bbde605db83a1f6b3ca05b66c5
Jeevan yadav Palle <jeevany841@gmail.com>
2025-11-04 23:22:19 +0530

    Create java3.java

---
+++ java3.java
@@ -0,0 +1,10 @@
+public class Main{
+public static void main (String[] args){int a = 10;
+double b = 15.5732;
+float c = 2;
+String helloJava = "hello,Java!";
+char d = helloJava;
+System.out.println(a+b);
+System.out.println(c);
+boolean check = a>b;
+System.out.println(check);}}
