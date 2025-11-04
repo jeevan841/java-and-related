a3034c75991dcbee7d9a8eb2fb0f39519ad86cc3
Jeevan yadav Palle <jeevany841@gmail.com>
2025-11-04 23:20:56 +0530

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,11 @@
+  public class Main {
+  public static void main(String[] args) {
+    int num = 6;
+    if (num%2.5==0)
+    	System.out.println("divisible by 2.5");
+    else if (num%5.25 == 0)
+    	System.out.println("divisible by 5.25");
+    else
+    	System.out.println("not divisible by 2.5 and 5.25");
+  }
+}
