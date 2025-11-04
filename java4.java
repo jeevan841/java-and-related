183d0b6349dc33fefccbeb8985722403b30a9d5e
Jeevan yadav Palle <jeevany841@gmail.com>
2025-11-04 23:38:13 +0530

    Create java4.java

---
+++ java4.java
@@ -0,0 +1,24 @@
+/*for loop
+public class Main{
+public static void main (String[] args){
+for(int i=1;i<=6;i++){
+System.out.println("Number:"+i);}}}
+*/
+/*while
+public class Main{
+public static void main (String[] args){
+int i=1;
+while(i<=6){
+System.out.println("Number:"+i);
+i++;}
+}}
+*/
+//do-while statement
+public class Main{
+public static void main (String[] args){
+int i=1;
+do{
+System.out.println("Number:"+i);
+i++;}
+while(i<=6);
+}}
