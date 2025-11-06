108bb3753218937b7bc4347d0117a4e8a153a509
Jeevan yadav Palle <jeevany841@gmail.com>
2025-11-07 00:00:24 +0530

    Create java6.java

---
+++ java6.java
@@ -0,0 +1,13 @@
+class MyClass{
+public static void main (String[] args){
+int numbers[]={10,20,30,40};
+int max = number[0];
+int min = number[0];
+for(int j=1;j<=number.length;j++){
+		if(number[i]>max){
+        	max=number[i];
+}
+		if(number[i]<min){
+        	min
+System.out.println("vowels:"+count);
+}}
