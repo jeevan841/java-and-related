5709835fac31ac95817477afba724c01818455c9
Jeevan yadav Palle <jeevany841@gmail.com>
2025-11-07 23:57:40 +0530

    Create java8.java

---
+++ starting/java8.java
@@ -0,0 +1,15 @@
+class circle{
+
+    double radius;
+    circle(double r){
+    	radius r;}
+        double area(){
+        return Math.PI*radius*2;
+    }
+
+    public static void main(String[] args) {
+        circle c1=new circle(5.0);
+        System.out.println(+c1.radius);
+        System.out.println(+c1.area());
+}
+}
